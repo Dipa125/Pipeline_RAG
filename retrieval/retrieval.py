e51e87628ded66d258573f5c9d68885651deca9f
Dipa125 <mat.dipangrazio@stud.uniroma3.it>
2025-01-23 14:09:19 +0000

    Sistemazione di un errore nel nome di una classe

--- retrieval/retrieval.py
+++ retrieval/retrieval.py
@@ -1,6 +1,6 @@
 from langchain.vectorstores import FAISS
 
-class Retriever:
+class Retrieval:
 
   def __new__(cls, vectorDB):
     return vectorDB.as_retriever(search_type="similarity", search_kwargs={"k": 4})
