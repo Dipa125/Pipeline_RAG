3beda1d05897f8440b3e22f363f3708d51dec1b4
Dipa125 <mat.dipangrazio@stud.uniroma3.it>
2025-01-23 11:17:22 +0000

    Implementazione della classe tokenizer e piccoli aggiustamenti per recognition

--- indexing/recognition.py
+++ indexing/recognition.py
@@ -1,7 +1,6 @@
-'''
-Questa classe si occupa del recupero dei file da una
-cartella e di convertirli in testo.
-'''
+"""
+Questa classe si occupa del recupero dei file da una cartella e di convertirli in testo.
+"""
 
 import os
 import fitz
@@ -13,7 +12,6 @@ import numpy as np
 class Recognition:
 
   def __init__(self, lang):
-    self.docs = {}
     self.paddleOCR = PaddleOCR(use_angle_cls=True, lang=lang)
 
 #-Concatena il testo rilevato in ogni pagina del PDF
@@ -31,8 +29,9 @@ class Recognition:
       text += "\n"
     return text
 
-#-
+#-Gestisce l'accesso ai file di una cartella ed avvia la giusta conversione
   def extractText(self, folder_path):
+    docs={}
     for file_name in os.listdir(folder_path):
 
       file_path = os.path.join(folder_path, file_name)
@@ -42,7 +41,7 @@ class Recognition:
       if file_path.endswith(".pdf"):
         text = self.__pdf2text(reader)
         if text.strip():
-          self.docs[file_name]=text
+          docs[file_name]=text
         else:
           images = []
           for page_num in range(len(reader)):
@@ -50,13 +49,13 @@ class Recognition:
               pix = page.get_pixmap(dpi=150)
               img = Image.open(BytesIO(pix.tobytes("png"))).convert("RGB")
               images.append(img)
-          self.docs[file_name] = self.__images2text(images)
+          docs[file_name] = self.__images2text(images)
 
       elif file_path.endswith((".png", ".jpg", ".jpeg", ".bmp")):
         img = Image.open(file_path).convert('RGB')
-        self.docs[file_name] = self.__images2text([img])
+        docs[file_name] = self.__images2text([img])
 
-    return self.docs
+    return docs
 
 
 
---
+++ indexing/tokenizer.py
@@ -0,0 +1,41 @@
+import sys
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from typing import Optional, List, Tuple
+from transformers import AutoTokenizer
+from sentence_transformers import SentenceTransformer
+
+sys.path.append('/content/Pipeline_RAG')
+from variables import EMBEDDING_MODEL_NAME
+from variables import MARKDOWN_SEPARATORS
+
+
+
+class Tokenizer:
+
+  def __init__(self):
+    max_chunk = SentenceTransformer(EMBEDDING_MODEL_NAME).max_seq_length
+    self.text_splitter = RecursiveCharacterTextSplitter.from_huggingface_tokenizer(
+        AutoTokenizer.from_pretrained(EMBEDDING_MODEL_NAME),
+        chunk_size=max_chunk,
+        chunk_overlap=int(max_chunk / 10),
+        add_start_index=True,
+        strip_whitespace=True,
+        separators=MARKDOWN_SEPARATORS,
+    )
+  
+  def split_documents(self, docs):
+    RAW_KNOWLEDGE_BASE = [
+      LangchainDocument(page_content=docs[doc], metadata={}) for doc in docs
+    ]
+    docs_processed = []
+    for doc in RAW_KNOWLEDGE_BASE:
+        docs_processed += self.text_splitter.split_documents([doc])
+
+    return docs_processed
+
+
+
+
+
+
+
