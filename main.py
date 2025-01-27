204d17db1aa9d98ebbe6b0166d4d625e4212a5cd
Dipa125 <mat.dipangrazio@stud.uniroma3.it>
2025-01-27 11:50:57 +0000

    Sistemazione del main per portare le classi in Colab

--- main.py
+++ main.py
@@ -10,26 +10,22 @@ def setup_environment():
       sys.path.append(PROGECT_PATH)
 
 def import_class():
+  global Recognition, Tokenizer, VectorDB_Manager, LoadModel, LLM, RAG
   from indexing.recognition import Recognition
   from indexing.tokenizer import Tokenizer
-
   from retrieval.vectorDB_Manager import VectorDB_Manager
-
   from postRetrieval.loadModel import LoadModel
   from postRetrieval.LLM import LLM
   from postRetrieval.RAG import RAG
 
-
-
-def main():
-  parser = argparse.ArgumentParser(description="--pathDoc per inserire il path contenente i documenti")
-  parser.add_argument("--pathDoc", type=str, required=True, help="path della cartella con i documenti")
-  args = parser.parse_args()
+# Funzione per l'avvio direttamente da Colab
+def create_chains(pathDoc=None):
+  if pathDoc is None:
+    raise ValueError("Il parametro 'pathDoc' è obbligatorio")
 
   setup_environment()
   import_class()
 
-"""
   # Istanziamento dell'OCR e del Tokenizzatore
   recognition = Recognition()
   tokenizer = Tokenizer()
@@ -45,14 +41,49 @@ def main():
   retrieval = vectorDB.retrieval()
 
   # Import del modello e tokenizzatore per LLM
-  model, tokenizer = LoadModel()
+  model, tokenizer = LoadModel(quantize=False)
 
   # Creazione della catena per LLM
   llm_chain = LLM(model, tokenizer)
 
   # Creazione della catena completa per la Pipeline RAG
   rag_chain = RAG(retrieval, llm_chain)
-"""
+
+  return llm_chain, rag_chain
+
+# Funzione main per l'avvio (per una gestione interattiva)
+def main():
+  parser = argparse.ArgumentParser(description="--pathDoc per inserire il path contenente i documenti")
+  parser.add_argument("--pathDoc", type=str, required=True, help="path della cartella con i documenti")
+  args = parser.parse_args()
+
+  setup_environment()
+  import_class()
+
+  # Istanziamento dell'OCR e del Tokenizzatore
+  recognition = Recognition()
+  tokenizer = Tokenizer()
+
+  # Processamento dei file all'interno di una cartella
+  docs = recognition.extractText(args.pathDoc)
+  docs_processed = tokenizer.split_documents(docs)
+
+  # Istanziamento della classe che gestisce il VectorDB
+  vectorDB = VectorDB_Manager(docs_processed)
+
+  # Creazione del retrieval associandolo al Vector DB
+  retrieval = vectorDB.retrieval()
+
+  # Import del modello e tokenizzatore per LLM
+  model, tokenizer = LoadModel(quantize=False)
+
+  # Creazione della catena per LLM
+  llm_chain = LLM(model, tokenizer)
+
+  # Creazione della catena completa per la Pipeline RAG
+  rag_chain = RAG(retrieval, llm_chain)
+
+  return llm_chain, rag_chain
 
 if __name__ == "__main__":
   main()
--- postRetrieval/loadModel.py
+++ postRetrieval/loadModel.py
@@ -4,13 +4,20 @@ from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig
 from variables import MODEL_NAME_BF16
 
 class LoadModel():
-  def __new__(cls):
-    bnb_config = BitsAndBytesConfig(
-      load_in_4bit=True,
-      bnb_4bit_use_double_quant=True,
-      bnb_4bit_quant_type="nf4",
-      bnb_4bit_compute_dtype=torch.bfloat16
-      )
-    model = AutoModelForCausalLM.from_pretrained(MODEL_NAME_BF16, quantization_config=bnb_config)
+  def __new__(cls, quantize=True):
+    if quantize:
+      bnb_config = BitsAndBytesConfig(
+        load_in_4bit=True,
+        bnb_4bit_use_double_quant=True,
+        bnb_4bit_quant_type="nf4",
+        bnb_4bit_compute_dtype=torch.bfloat16
+        )
+      model = AutoModelForCausalLM.from_pretrained(
+        MODEL_NAME_BF16,
+        quantization_config=bnb_config
+        )
+    else:
+      model = AutoModelForCausalLM.from_pretrained(MODEL_NAME_BF16)
+
     tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME_BF16)
     return model, tokenizer
