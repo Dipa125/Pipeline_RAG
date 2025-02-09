e3ceb862e37b6e0db854d9d097793a7b864c2a99
Dipa125 <mat.dipangrazio@stud.uniroma3.it>
2025-02-09 13:33:40 +0000

    Modifica del funzionametno per caricare gli LLM

--- postRetrieval/LLM.py
+++ postRetrieval/LLM.py
@@ -1,34 +1,54 @@
 from transformers import pipeline
 
 from langchain.prompts import PromptTemplate
-from langchain_huggingface import HuggingFacePipeline
+from langchain_huggingface import HuggingFacePipeline, HuggingFaceEndpoint
 from langchain_core.output_parsers import StrOutputParser
+from langchain_openai import ChatOpenAI
+
+from postRetrieval.loadModel import LoadModel
 
 from variables import PROMT_TEMPLATE_ITA
 from variables import PROMT_TEMPLATE_ENG
 
 # Gestione diversa per la costruzione del prompt?
+# Gestiore parametri diversi per LLM
 class LLM:
-  def __new__(cls, model, tokenizer=None):
-    if tokenizer is None:
-      llm = model
+  def __new__(cls, model_name, is_local=False, quantize = False, key_HF=None, key_GPT=None, lang="en"):
+    if not is_local and (key_HF is None and key_GPT is None):
+        raise ValueError("To use a remote model, you must provide a valid key.")
+    
+    if is_local:
+        model, tokenizer = LoadModel(model_name, quantize)
+        text_generation_pipeline = pipeline(
+            model=model,
+            tokenizer=tokenizer,
+            task="text-generation",
+            temperature=0.2,
+            do_sample=True,
+            repetition_penalty=1.1,
+            return_full_text=True,
+            max_new_tokens=300,
+            pad_token_id=tokenizer.eos_token_id
+            )
+        llm = HuggingFacePipeline(pipeline=text_generation_pipeline)
     else:
-      text_generation_pipeline = pipeline(
-        model=model,
-        tokenizer=tokenizer,
-        task="text-generation",
-        temperature=0.2,
-        do_sample=True,
-        repetition_penalty=1.1,
-        return_full_text=True,
-        max_new_tokens=300,
-        pad_token_id=tokenizer.eos_token_id
-        )
-      llm = HuggingFacePipeline(pipeline=text_generation_pipeline)
+        if key_HF is not None:
+            llm = HuggingFaceEndpoint(
+                task="text-generation",
+                repo_id = model_name,
+                huggingfacehub_api_token = key_HF,
+                temperature=0.2,
+                do_sample=True,
+                repetition_penalty=1.1,
+                return_full_text=True,
+                max_new_tokens=300,
+            )
+        else:
+            llm = ChatOpenAI(model="gpt-3.5-turbo", openai_api_key=key_GPT)
       
     prompt = PromptTemplate(
       input_variables=["context", "question"],
-      template = PROMT_TEMPLATE_ITA,
+      template = PROMT_TEMPLATE_ITA if lang=="ita" else PROMT_TEMPLATE_ENG,
       )   
     
     return prompt | llm | StrOutputParser()
--- postRetrieval/loadModel.py
+++ postRetrieval/loadModel.py
@@ -1,37 +1,30 @@
 import torch
 from transformers import AutoTokenizer, AutoModelForCausalLM 
 from transformers import BitsAndBytesConfig
-from langchain_openai import ChatOpenAI
 
-from variables import MODEL_NAME_FACEBOOK
-
+# Parametrizzare la quantizzazione?
 class LoadModel():
 
-  def __new__(cls, api_key=None, quantize=True):
-    if api_key is not None:
-      model = ChatOpenAI(model="gpt-3.5-turbo", openai_api_key=api_key)
-      return model, None
-
+  def __new__(cls, model_name, quantize):
+    if quantize:
+      bnb_config = cls._quantize_4bit()
+      model = AutoModelForCausalLM.from_pretrained(
+        model_name,
+        quantization_config=bnb_config
+      )
     else:
-      if quantize:
-        bnb_config = cls._quantize_4bit()
-        model = AutoModelForCausalLM.from_pretrained(
-          MODEL_NAME_FACEBOOK,
-          quantization_config=bnb_config
-          )
-      else:
-        model = AutoModelForCausalLM.from_pretrained(MODEL_NAME_FACEBOOK)
-        
-      tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME_FACEBOOK)
-      return model, tokenizer
+      model = AutoModelForCausalLM.from_pretrained(model_name)
+      
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    return model, tokenizer
 
   def _quantize_4bit():
     return BitsAndBytesConfig(
-          load_in_4bit=True,
-          bnb_4bit_use_double_quant=True,
-          bnb_4bit_quant_type="nf4",
-          bnb_4bit_compute_dtype=torch.bfloat16
-          )
+      load_in_4bit=True,
+      bnb_4bit_use_double_quant=True,
+      bnb_4bit_quant_type="nf4",
+      bnb_4bit_compute_dtype=torch.bfloat16
+    )
 
 
 
