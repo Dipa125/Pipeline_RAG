965478022a12bd590dcc051432f2fcc5cf3d654f
Dipa125 <mat.dipangrazio@stud.uniroma3.it>
2025-02-22 17:50:46 +0000

    Risolti alcuni porblemi sulla RAG con memoria

--- postRetrieval/RAG.py
+++ postRetrieval/RAG.py
@@ -6,37 +6,33 @@ from langchain.memory import ConversationBufferMemory
 
 from variables import Prompt_Template
 
-class RAG:
-  def __new__(cls, retrieval, llm_chain):
-    return {"context": retrieval, "question": RunnablePassthrough()} | llm_chain
-
 # Rivedere i prompt
 class RAG_Builder:
-  def __init__(self, llm, retrieval):
+  def __init__(self, llm, vectorDB):
     self.llm = llm
-    self.retrieval = retrieval
+    self.vectorDB = vectorDB
 
   def build_RAG_without_context(self):
     prompt = PromptTemplate(
       input_variables=["question"],
-      template = Prompt_Template.NO_CONTEXT,
+      template = Prompt_Template.NO_CONTEXT.value,
     )
     return prompt | self.llm | StrOutputParser()
   
   def build_RAG_with_context(self):
     prompt = PromptTemplate(
       input_variables=["question"],
-      template = Prompt_Template.EXTENDED_CONTEXT,
+      template = Prompt_Template.EXTENDED_CONTEXT.value,
     )
-    return {"context": self.retrieval, "question": RunnablePassthrough()} | prompt | self.llm | StrOutputParser()
+    return {"context": self.vectorDB.retrieval, "question": RunnablePassthrough()} | prompt | self.llm | StrOutputParser()
 
   def build_RAG_with_chat(self, use_memory=False):
     memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True) if use_memory else None
     prompt = PromptTemplate(
       input_variables=["question"],
-      template = Prompt_Template.REDUCED_CONTEXT,
+      template = Prompt_Template.REDUCED_CONTEXT.value,
     )
-    return ChatVectorDBChain.from_llm(self.llm, self.retrieval, memory=memory, question_prompt=prompt)
+    return ChatVectorDBChain.from_llm(self.llm, self.vectorDB, memory=memory, condense_question_prompt=prompt)
     
 
 
